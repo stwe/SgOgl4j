db547123861de5927a9c5121f857e29118a11f7a
stwe <stwe@nomail.tt>
2020-09-09 21:24:40 +0200

    update headers

--- src/main/java/de/sg/ogl/Application.java
+++ src/main/java/de/sg/ogl/Application.java
@@ -1,3 +1,11 @@
+/*
+ * This file is part of the SgOgl4j project.
+ *
+ * Copyright (c) 2020. stwe <https://github.com/stwe/SgOgl4j>
+ *
+ * License: MIT
+ */
+
 package de.sg.ogl;
 
 public interface Application {
--- src/main/java/de/sg/ogl/buffer/VertexAttribute.java
+++ src/main/java/de/sg/ogl/buffer/VertexAttribute.java
@@ -1,3 +1,11 @@
+/*
+ * This file is part of the SgOgl4j project.
+ *
+ * Copyright (c) 2020. stwe <https://github.com/stwe/SgOgl4j>
+ *
+ * License: MIT
+ */
+
 package de.sg.ogl.buffer;
 
 import static org.lwjgl.opengl.GL11.GL_FLOAT;
