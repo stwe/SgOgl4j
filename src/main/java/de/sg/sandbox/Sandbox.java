d23e884fdaff66354c4497cc3bb351cb3e927a99
stwe <stwe@nomail.tt>
2020-09-14 18:31:33 +0200

    read bsh

--- src/main/java/de/sg/sandbox/Sandbox.java
+++ src/main/java/de/sg/sandbox/Sandbox.java
@@ -10,29 +10,22 @@ package de.sg.sandbox;
 
 import de.sg.ogl.BaseApplication;
 import de.sg.ogl.Input;
-import de.sg.ogl.camera.FirstPersonCamera;
-import org.joml.Vector3f;
 import org.lwjgl.glfw.GLFW;
-
 import java.io.IOException;
 
 import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
 
 public class Sandbox extends BaseApplication {
 
-    LineRenderer renderer;
-    FirstPersonCamera camera;
+    BshReader bshReader;
 
     public Sandbox() throws IOException, IllegalAccessException {
     }
 
     @Override
     public void init() throws Exception {
-        camera = new FirstPersonCamera();
-        camera.setPosition(new Vector3f(0.0f, 0.0f, 1.0f));
-        camera.setYaw(-90.0f);
-
-        renderer = new LineRenderer(getEngine(), camera);
+        bshReader = new BshReader("/bsh/STADTFLD.BSH");
+        bshReader.load();
     }
 
     @Override
@@ -40,18 +33,14 @@ public class Sandbox extends BaseApplication {
         if (Input.isKeyDown(GLFW.GLFW_KEY_ESCAPE)) {
             glfwSetWindowShouldClose(getEngine().getWindow().getWindowHandle(), true);
         }
-
-        camera.input();
     }
 
     @Override
     public void update(float dt) {
-        camera.update(dt);
     }
 
     @Override
     public void render() {
-        renderer.render();
     }
 
     @Override
