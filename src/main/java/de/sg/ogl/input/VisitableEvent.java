8198eabf7992923b5270e09e2473e230dc56d0f4
stwe <stwe@nomail.tt>
2021-01-14 21:06:48 +0100

    visitable event base class

---
+++ src/main/java/de/sg/ogl/input/VisitableEvent.java
@@ -0,0 +1,13 @@
+/*
+ * This file is part of the SgOgl4j project.
+ *
+ * Copyright (c) 2021. stwe <https://github.com/stwe/SgOgl4j>
+ *
+ * License: MIT
+ */
+
+package de.sg.ogl.input;
+
+public abstract class VisitableEvent implements Visitable {
+    EventType eventType = EventType.NONE;
+}
